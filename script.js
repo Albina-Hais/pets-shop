a9dee740fe0a096c650aaf66c63589c45feb7d70
Albina-Hais <albina.hais@yandex.by>
2025-04-01 21:56:29 +0300

    completed tasks

--- script.js
+++ script.js
@@ -1,86 +1,127 @@
-const items = [
-  {
-    title: "Игрушка мячик",
-    description: "Ваш питомец будет счастлив!",
-    tags: ["cat", "dog"],
-    price: 500,
-    img: "./img/1.jpeg",
-  },
-  {
-    title: "Игрушка лабиринт",
-    description: "Поможет в развитии интеллекта!",
-    tags: ["cat", "dog"],
-    price: 900,
-    img: "./img/2.jpeg",
-  },
-  {
-    title: "Игрушка для котят",
-    description: "Отвлечет вашего питомца!",
-    tags: ["cat"],
-    price: 300,
-    img: "./img/3.jpeg",
-  },
-  {
-    title: "Миска «Котик»",
-    description: "Подойдет и для собак!",
-    tags: ["cat", "dog"],
-    price: 660,
-    img: "./img/4.jpeg",
-  },
-  {
-    title: "Лоток розовый",
-    description: "Теперь вы можете забыть о проблемах с туалетом",
-    tags: ["cat"],
-    price: 400,
-    img: "./img/5.jpeg",
-  },
-  {
-    title: "Сухой корм для кошек",
-    description: "Специальная формула для милых усатиков!",
-    tags: ["cat"],
-    price: 200,
-    img: "./img/6.jpeg",
-  },
-  {
-    title: "Сухой корм для собак",
-    description: "Содержит полный комплекс витаминов",
-    tags: ["dog"],
-    price: 300,
-    img: "./img/7.jpeg",
-  },
-  {
-    title: "Игрушка для собак",
-    description: "Теперь вы можете не переживать за личные вещи",
-    tags: ["dog"],
-    price: 500,
-    img: "./img/8.jpeg",
-  },
-  {
-    title: "Лежанка",
-    description: "Идеальное место для отдыха!",
-    tags: ["cat", "dog"],
-    price: 1500,
-    img: "./img/9.jpeg",
-  },
-  {
-    title: "Поилка для собак",
-    description: "Возьмите с собой в путешествие",
-    tags: ["dog"],
-    price: 800,
-    img: "./img/10.jpeg",
-  },
-  {
-    title: "Переноска",
-    description: "Путешествуйте с комфортом!",
-    tags: ["cat", "dog"],
-    price: 3500,
-    img: "./img/11.jpeg",
-  },
-  {
-    title: "Поводок для собак",
-    description: "Для чудесных прогулок вместе",
-    tags: ["dog"],
-    price: 800,
-    img: "./img/12.jpeg",
-  },
+const items = [{
+        title: "Игрушка мячик",
+        description: "Ваш питомец будет счастлив!",
+        tags: ["cat", "dog"],
+        price: 500,
+        img: "./img/1.jpeg",
+    },
+    {
+        title: "Игрушка лабиринт",
+        description: "Поможет в развитии интеллекта!",
+        tags: ["cat", "dog"],
+        price: 900,
+        img: "./img/2.jpeg",
+    },
+    {
+        title: "Игрушка для котят",
+        description: "Отвлечет вашего питомца!",
+        tags: ["cat"],
+        price: 300,
+        img: "./img/3.jpeg",
+    },
+    {
+        title: "Миска «Котик»",
+        description: "Подойдет и для собак!",
+        tags: ["cat", "dog"],
+        price: 660,
+        img: "./img/4.jpeg",
+    },
+    {
+        title: "Лоток розовый",
+        description: "Теперь вы можете забыть о проблемах с туалетом",
+        tags: ["cat"],
+        price: 400,
+        img: "./img/5.jpeg",
+    },
+    {
+        title: "Сухой корм для кошек",
+        description: "Специальная формула для милых усатиков!",
+        tags: ["cat"],
+        price: 200,
+        img: "./img/6.jpeg",
+    },
+    {
+        title: "Сухой корм для собак",
+        description: "Содержит полный комплекс витаминов",
+        tags: ["dog"],
+        price: 300,
+        img: "./img/7.jpeg",
+    },
+    {
+        title: "Игрушка для собак",
+        description: "Теперь вы можете не переживать за личные вещи",
+        tags: ["dog"],
+        price: 500,
+        img: "./img/8.jpeg",
+    },
+    {
+        title: "Лежанка",
+        description: "Идеальное место для отдыха!",
+        tags: ["cat", "dog"],
+        price: 1500,
+        img: "./img/9.jpeg",
+    },
+    {
+        title: "Поилка для собак",
+        description: "Возьмите с собой в путешествие",
+        tags: ["dog"],
+        price: 800,
+        img: "./img/10.jpeg",
+    },
+    {
+        title: "Переноска",
+        description: "Путешествуйте с комфортом!",
+        tags: ["cat", "dog"],
+        price: 3500,
+        img: "./img/11.jpeg",
+    },
+    {
+        title: "Поводок для собак",
+        description: "Для чудесных прогулок вместе",
+        tags: ["dog"],
+        price: 800,
+        img: "./img/12.jpeg",
+    },
 ];
+
+
+const shopItemsContainer = document.getElementById("shop-items");
+
+items.forEach(item => {
+
+    const shopItem = document.createElement('div');
+    shopItem.className = 'shop-item';
+
+    const img = document.createElement('img');
+    img.src = item.img;
+
+    const content = document.createElement('div');
+    content.className = 'content';
+
+    const tagsDiv = document.createElement('div');
+    tagsDiv.className = 'tags';
+    item.tags.forEach(tag => {
+        const tagSpan = document.createElement('span');
+        tagSpan.textContent = tag;
+        tagsDiv.appendChild(tagSpan);
+    });
+
+    const title = document.createElement('h1');
+    title.textContent = item.title;
+
+    const description = document.createElement('p');
+    description.textContent = item.description;
+
+    const price = document.createElement('span');
+    price.className = 'price';
+    price.textContent = item.price;
+
+    content.appendChild(tagsDiv);
+    content.appendChild(title);
+    content.appendChild(description);
+    content.appendChild(price);
+    shopItem.appendChild(img);
+    shopItem.appendChild(content);
+
+    shopItemsContainer.appendChild(shopItem);
+});
